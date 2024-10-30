82c8ce0dfb6fbd14600fb656d24ad3c0f4677a77
Allisson100 <allisson.boiane@outlook.com>
2024-10-30 12:58:22 -0300

    first commit

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -5,12 +5,12 @@ Future initFirebase() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: FirebaseOptions(
-            apiKey: "AIzaSyBHLbae9sNhYVHpC8l-BDknMTWia99tx_I",
-            authDomain: "ong---voluntario.firebaseapp.com",
-            projectId: "ong---voluntario",
-            storageBucket: "ong---voluntario.appspot.com",
-            messagingSenderId: "184565750636",
-            appId: "1:184565750636:web:7abde66c38a62acf70f149"));
+            apiKey: "",
+            authDomain: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: "",
+            appId: ""));
   } else {
     await Firebase.initializeApp();
   }
